3afc9f381be28e44fc3e16268eccb5582d2689d0
Adam28102006 <A00023435@myTUDublin.ie>
2026-02-23 15:36:32 +0000

    Initialize Supabase client in script.js
    Hopefully this works, had to look this up

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-test
+
+const supabaseUrl = 'https://supabase.com/dashboard/project/jeuedjgfkliemobkvvjb/sql/aa7c4048-0b03-4225-912e-86b04074eb1e';
+const supabaseKey = 'sb_publishable_1CcmYDiX_riiirF8PpFd_w_d4fzUBoZ';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
