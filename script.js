bcb50e8da75c16accd6c44d847313ca2ddeabd90
Adam28102006 <A00023435@myTUDublin.ie>
2026-03-17 13:29:10 +0000

    Update script.js
    Put the Wrong Supabase link in there

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-const supabaseUrl = 'https://supabase.com/dashboard/project/jeuedjgfkliemobkvvjb/sql/aa7c4048-0b03-4225-912e-86b04074eb1e';
+const supabaseUrl = https://jeuedjgfkliemobkvvjb.supabase.co';
 const supabaseKey = 'sb_publishable_1CcmYDiX_riiirF8PpFd_w_d4fzUBoZ';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
